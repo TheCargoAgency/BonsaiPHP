fa1094a55349b4c74b829e808c6315ac50a712e6
Andy Benedict <dev@andybenedict.com>
2015-01-09 13:44:58 -0500

    correct constant path - remove redundant slash

--- src/Module/Registry.php
+++ src/Module/Registry.php
@@ -94,7 +94,7 @@ class Registry
     
     private function defineConstants(){
         defined('Bonsai\DOCUMENT_ROOT') || define('Bonsai\DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
-        defined('Bonsai\PROJECT_ROOT') || define('Bonsai\PROJECT_ROOT', __DIR__ . '/../');
+        defined('Bonsai\PROJECT_ROOT') || define('Bonsai\PROJECT_ROOT', __DIR__ . '/..');
         defined('Bonsai\SERVER_ROOT') || define('Bonsai\SERVER_ROOT', '');
     }
 
